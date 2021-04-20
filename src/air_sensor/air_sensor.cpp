4b535b7fae7ac88ae1e0a51fa6ab8f961e3be5cd
Yurii <Yurri2002@gmail.com>
2021-04-20 16:52:53 +0300

    changed air sensor and added lib

--- src/air_sensor/air_sensor.cpp
+++ src/air_sensor/air_sensor.cpp
@@ -1,17 +1,36 @@
+#include "Arduino.h"
 #include "air_sensor.h"
+#include "stDHT.h"
 
 namespace air_sensor {
 
     void initialize() {
-        // TODO
+        // TODO: check operability
+        DHT sens(DHT11);
+        pinMode(PIN_NUMBER, INPUT);
+        digitalWrite(PIN_NUMBER, HIGH);
     }
 
-    void get_air_humidity() {
-
+    state get_air_humidity() {
+        int hum = sens.readHumidity(PIN_NUMBER);
+        if (hum <= LOWER_HUMIDITY_BOUND){
+            return NOT_ENOUGH_HUMIDITY; 
+        }
+        else if (hum >= UPPER_HUMIDITY_BOUND) {
+            return HIGH_HUMIDITY;
+        }
+        else return NORMAL_HUMIDITY;
     }
 
-    void get_air_temperature() {
-
+    state get_air_temperature() {
+        int temp = sens.readTemperature(PIN_NUMBER);
+        if (temp < LOWER_TEMPERATURE_BOUND) {
+            return NOT_ENOUGH_TEMPERATURE;
+        }
+        else if (temp > UPPER_TEMPERATUR_BOUND) {
+            return HIGH_TEMPERATURE;
+        }
+        else return NORMAL_TEMPERATURE;
     }
 
 }
--- src/air_sensor/air_sensor.h
+++ src/air_sensor/air_sensor.h
@@ -2,10 +2,22 @@
 
 namespace air_sensor {
 
+    //todo: change temperature and humidity bounds
+    const int PIN_NUMBER = 3;
+
+    const int LOWER_TEMPERATURE_BOUND = 5;
+    const int UPPER_TEMPERATUR_BOUND = 25;
+
+    const int LOWER_HUMIDITY_BOUND = 40;
+    const int UPPER_HUMIDITY_BOUND = 55;
+
+    enum state { NOT_ENOUGH_HUMIDITY, HIGH_HUMIDITY, NORMAL_HUMIDITY, NOT_ENOUGH_TEMPERATURE,
+        HIGH_TEMPERATURE, NORMAL_TEMPERATURE};
+
     void initialize();
 
-    void get_air_humidity();
+    state get_air_humidity();
 
-    void get_air_temperature();
+    state get_air_temperature();
 
 }
